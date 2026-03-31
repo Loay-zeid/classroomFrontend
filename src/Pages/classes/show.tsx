0397f51e68d559cd22740792d3bcd377d398328f
Loay-zeid <loayze007@gmail.com>
2026-04-01 02:15:37 +0300

    fix classes show details

--- src/Pages/classes/show.tsx
+++ src/Pages/classes/show.tsx
@@ -3,6 +3,9 @@ import {useShow} from "@refinedev/core";
 import {ClassDetails} from "@/types";
 import {ShowView, ShowViewHeader} from "@/components/refine-ui/views/show-view.tsx";
 import {Card} from "@/components/ui/card.tsx";
+import {Badge} from "@/components/ui/badge.tsx";
+import {Separator} from "@/components/ui/separator.tsx";
+import {Button} from "@/components/ui/button.tsx";
 
 const Show = () => {
 
@@ -10,7 +13,7 @@ const Show = () => {
 
     const classDetails=query.data?.data;
 
-    const {data,isLoading,isError}=query;
+    const { isLoading, isError } = query;
 
     if(isLoading || isError || !classDetails){
         return (
@@ -35,6 +38,7 @@ const Show = () => {
 
     const placeholderUrl = `https://placehold.co/600x400?text=${encodeURIComponent(teacherInitials || 'NA')}`;
 
+    const { name, description, status, capacity, bannerUrl, subject, teacher, department } = classDetails;
 
     return (
         <ShowView className="class-view class-show">
@@ -42,20 +46,74 @@ const Show = () => {
 
             <div className="banner">
                 {
-                    classDetails.bannerUrl ?  <p>Render Clodinary's advanced image</p>:
+                    bannerUrl ?  <p>Render Cloudinary's advanced image</p>:
                         <div className="placeholder"/>
                 }
             </div>
 
-            <Card className="details-header">
-                <div>
-                    <h1>
-                        {classDetails.name}
-                    </h1>
-                    <p>
-                        {classDetails.description}
-                    </p>
+            <Card className="details-card">
+                <div className="details-header">
+                    <div>
+                        <h1>
+                            {name}
+                        </h1>
+                        <p>
+                            {description}
+                        </p>
                 </div>
+                    <div>
+                        <Badge variant="outline" >{capacity} spots</Badge>
+                        <Badge variant={status == 'active'?'default' : 'secondary'} data-status={status}>{status.toUpperCase()}</Badge>
+                    </div>
+                </div>
+                <div className="details-grid">
+                    <div className="instructor">
+                        <p>
+                            instructor
+                        </p>
+                        <div>
+                            <img src={teacher?.image ?? placeholderUrl} alt={teacherName} />
+                            <div>
+                                <p>{teacherName}</p>
+                                <p>{teacher?.email}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="department">
+                        <p>Department</p>
+
+                        <div>
+                           <p> {department?.name} </p>
+                            <p> {department?.description}</p>
+                        </div>
+                    </div>
+                </div>
+
+                <Separator/>
+
+                <div className="subject">
+                    <p>Subject</p>
+                    <div>
+                        <Badge variant="outline">Code: {subject?.courseCode}</Badge>
+                        <p>{subject?.name}</p>
+                        <p>{subject?.briefDescription}</p>
+                    </div>
+                </div>
+
+                <Separator/>
+                <div className="join">
+                    <h2>Join Class</h2>
+
+                    <ol>
+                        <li>Ask your teacher for the invite code </li>
+                        <li>Click on "join class" button </li>
+                        <li>Paste the code and click "join" </li>
+
+                    </ol>
+                </div>
+                <Button className="w-full" size="lg">
+                    Join Class
+                </Button>
             </Card>
         </ShowView>
     )
